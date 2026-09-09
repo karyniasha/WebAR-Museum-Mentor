935d525e2cf0d46251463279f36d91170be4522d
Igor <karyniasha2@mail.com>
2026-09-09 20:46:26 +0400

    chore: configure Gracia Vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
-export default {
+import { defineConfig } from 'vite'
+import { graciaPlugin } from '@gracia/web-sdk/vite-plugin'
+
+export default defineConfig({
   base: '/WebAR-Museum-Mentor/',
-}
+  plugins: [
+    ...graciaPlugin({
+      bundle: 'core',
+      dedupe: true,
+    }),
+  ],
+})
